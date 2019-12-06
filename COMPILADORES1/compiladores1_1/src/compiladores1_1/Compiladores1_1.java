c7faa5a12175d4de682b6d24a581c2ce45070698
David <201709454@ingenieria.usac.edu.gt>
2019-12-06 16:46:48 -0600

    ya funciona el intérprete básico

--- COMPILADORES1/compiladores1_1/src/compiladores1_1/Compiladores1_1.java
+++ COMPILADORES1/compiladores1_1/src/compiladores1_1/Compiladores1_1.java
@@ -6,25 +6,45 @@
 package compiladores1_1;
 
 import arbol.AST;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.StringReader;
 
 /**
  *
  * @author David Ventura
  */
 public class Compiladores1_1 {
-
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        
-        
-    }
     
-    public static void interpretar (String path) {
-        
+    public static fmrPrincipal fmrP ;
+    public static void main(String[] args) {
+        interpretar("Entradas/entrada1.txt");
+    }
+
+    private static void interpretar(String path) {
+        analizadores.parser pars;
+//        Instruccion bloque;
         AST arbol;
+        try {
+            pars = new analizadores.parser(new analizadores.Lexico(new FileInputStream(path)));
+            pars.parse();
+            arbol = pars.AST;
+
+            if (arbol != null) { //Si no existió un error en el análisis
+                arbol.Ejecutar();
+            } else {
+                System.out.println("<----------> Existió un error en el análisis, no se pudo construir el árbol <---------->");
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+            System.out.println("Error fatal en compilación de entrada.");
+        }
+        
         
         
         
