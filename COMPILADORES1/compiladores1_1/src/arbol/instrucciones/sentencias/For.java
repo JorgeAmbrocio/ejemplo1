0a00432d04ba7f3acd9f159e73378ddc5e76084c
David <201709454@ingenieria.usac.edu.gt>
2019-12-13 12:59:48 -0600

    for teminado

--- COMPILADORES1/compiladores1_1/src/arbol/instrucciones/sentencias/For.java
+++ COMPILADORES1/compiladores1_1/src/arbol/instrucciones/sentencias/For.java
@@ -5,8 +5,14 @@
  */
 package arbol.instrucciones.sentencias;
 
+import arbol.Expresion;
 import arbol.Instruccion;
 import arbol.entorno.Entorno;
+import arbol.entorno.Tipo;
+import arbol.instrucciones.Asignacion;
+import arbol.instrucciones.Break;
+import arbol.instrucciones.Continue;
+import compiladores1_1.Compiladores1_1;
 
 /**
  *
@@ -14,16 +20,68 @@ import arbol.entorno.Entorno;
  */
 public class For  extends Instruccion{
 
+    Instruccion inicializacion;
+    Expresion condicion;
+    Instruccion actualizacion;
     Bloque bloque;
-    
+
+    public For(Instruccion inicializacion, Expresion condicion, Instruccion actualizacion, Bloque bloque) {
+        this.inicializacion = inicializacion;
+        this.condicion = condicion;
+        this.actualizacion = actualizacion;
+        this.bloque = bloque;
+    }
     
     
     @Override
     public Object ejecutar(Entorno ent) {
+        compiladores1_1.Compiladores1_1.pilaCiclos.addLast(Compiladores1_1.enumCiclo.Ciclo);
+        Entorno entCondiciones = new Entorno (ent);
+        Object retorno;
         
+        // ejecutar la inicialización de ciclo for
+        this.inicializacion.ejecutar(entCondiciones);
         
+        // ejecutar la condicion
+        Expresion condicion_ = this.condicion.getValor(entCondiciones);
         
+        // validar si se cumple la condicion
+        if (condicion_.tipo.tipo == Tipo.EnumTipo.booleano) {
+            // sí es un valor booleano
+            
+            boolean condicion__ = Boolean.parseBoolean(condicion_.valor.toString());
+            while (condicion__) {
+                // sí es válida la condición, ejecutar el bloque en un entorno nuevo
+                Entorno entBloque = new Entorno (entCondiciones);
+                retorno = this.bloque.ejecutar(entBloque);
+                
+                /// verificar si viene un break o un contirnue
+                if  (retorno != null ) {
+                    
+                    if (retorno.getClass() == Break.class) {
+                        // si viene un break, se debe terminar el prceso del for
+                        break;
+                    }else if (retorno.getClass() == Continue.class) {
+                        // si viene un continue, no se hace nada
+                        
+                    }        
+                }
+                
+                // luego de ejecutar el bloque
+                // ejecutar la actualización en el entorno de condiciones
+                this.actualizacion.ejecutar(entCondiciones);
+                
+                // revalidar la condicion
+                condicion_ = this.condicion.getValor(entCondiciones);
+                condicion__ = Boolean.parseBoolean(condicion_.valor.toString());
+                
+            }
+            
+        }else {
+            System.out.println("Se esperaba una expresion booleana en la condición del for");
+        }
         
+        compiladores1_1.Compiladores1_1.pilaCiclos.pollLast();
         return null;
     }
     
