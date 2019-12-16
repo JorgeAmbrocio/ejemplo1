0949024be458b414cfb2b4e52902af484328db81
David <201709454@ingenieria.usac.edu.gt>
2019-12-15 23:21:15 -0600

    decremento

---
+++ COMPILADORES1/compiladores1_1/src/arbol/expresiones/aritmeticos/ExpDecremento.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbol.expresiones.aritmeticos;
+
+import arbol.Expresion;
+import arbol.entorno.Entorno;
+import arbol.entorno.Simbolo;
+import arbol.entorno.Tipo;
+import arbol.expresiones.Literal;
+import arbol.instrucciones.Asignacion;
+
+/**
+ *
+ * @author David Ventura
+ */
+public class ExpDecremento extends Expresion {
+    String id;
+
+    public ExpDecremento(String id, int linea, int columna) {
+        this.id = id;
+        this.linea = linea;
+        this.columna= columna;
+    }
+    
+    
+    @Override
+    public Expresion getValor(Entorno ent) {
+        
+        Literal retorno  = null;
+        Simbolo e = ent.buscar(id, linea, columna, "La variable"); //Busco la variable en la tabla de símbolos
+        Asignacion a;
+        switch (e.tipo.tipo) {
+            case entero:
+               int valor = Integer.parseInt(e.valor.toString());
+               retorno = new Literal (new Tipo (Tipo.EnumTipo.entero) , valor);
+               valor--;
+               a = new Asignacion (this.id , this.linea , this.columna , new Literal(new Tipo (Tipo.EnumTipo.entero) , valor ));
+               a.ejecutar(ent);
+               break;
+            case doble:
+                double valor2 = Double.parseDouble(e.valor.toString());
+                retorno = new Literal (new Tipo (Tipo.EnumTipo.doble) , valor2);
+                valor2--; 
+                a = new Asignacion (this.id , this.linea , this.columna , new Literal(new Tipo (Tipo.EnumTipo.doble) , valor2 ));
+                a.ejecutar(ent);
+                break;
+            case caracter:
+                char valor3 = e.valor.toString().charAt(0);
+                retorno = new Literal (new Tipo (Tipo.EnumTipo.caracter) , valor3);
+                valor3 -- ;
+                a = new Asignacion (this.id , this.linea , this.columna , new Literal(new Tipo (Tipo.EnumTipo.caracter) , valor3 ));
+                a.ejecutar(ent);
+                break;
+        }
+        
+        if (retorno == null) {
+            System.out.println("Error");
+        }
+        
+        return retorno;
+    }
+
+}
---
+++ COMPILADORES1/compiladores1_1/src/arbol/instrucciones/InsDecremento.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbol.instrucciones;
+
+import arbol.Instruccion;
+import arbol.entorno.Entorno;
+import arbol.entorno.Simbolo;
+import arbol.entorno.Tipo;
+import arbol.expresiones.Literal;
+
+/**
+ *
+ * @author David Ventura
+ */
+public class InsDecremento extends Instruccion {
+    String id;
+
+    public InsDecremento(String id, int linea, int columna) {
+        this.id = id;
+        this.linea = linea;
+        this.columna= columna;
+    }
+    
+    
+    @Override
+    public Object ejecutar(Entorno ent) {
+        
+        Object retorno  = null;
+        Simbolo e = ent.buscar(id, linea, columna, "La variable"); //Busco la variable en la tabla de símbolos
+        Asignacion a;
+        switch (e.tipo.tipo) {
+            case entero:
+               int valor = Integer.parseInt(e.valor.toString());
+               valor--;
+               a = new Asignacion (this.id , this.linea , this.columna , new Literal(new Tipo (Tipo.EnumTipo.entero) , valor ));
+               a.ejecutar(ent);
+               return null;
+            case doble:
+                double valor2 = Double.parseDouble(e.valor.toString());
+                valor2--; 
+                a = new Asignacion (this.id , this.linea , this.columna , new Literal(new Tipo (Tipo.EnumTipo.doble) , valor2 ));
+                a.ejecutar(ent);
+                return null;
+            case caracter:
+                char valor3 = e.valor.toString().charAt(0);
+                valor3 -- ;
+                a = new Asignacion (this.id , this.linea , this.columna , new Literal(new Tipo (Tipo.EnumTipo.caracter) , valor3 ));
+                a.ejecutar(ent);
+                return null;
+        }
+        
+        System.out.println("error");
+        return null;
+    }
+}
